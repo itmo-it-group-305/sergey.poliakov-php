a2633039e4cbfd04e3f3e0087f23eca8b3d28c68
Sergey Poliakov <ps.force@gmail.com>
2016-01-18 15:44:59 +0300

    Убрал вспомогательную переменную

--- hometask/02-15012016/backtoarray.php
+++ hometask/02-15012016/backtoarray.php
@@ -10,9 +10,12 @@ $myArray = [1, 2, 3, 8, 14, 89, 45];
 $length = count($myArray) - 1;
 
 for ($i = 0; $i <= $length/2; $i++) {
-    $temp = $myArray[$i]; # @fixme: нельзя использовать вспомогательную переменную =)
-    $myArray[$i] = $myArray[$length - $i];
-    $myArray[$length - $i] = $temp;
+    $myArray[$i] = $myArray[$i] + $myArray[$length-$i];
+    $myArray[$length-$i] = $myArray[$i] - $myArray[$length-$i];
+    $myArray[$i] = $myArray[$i] - $myArray[$length-$i];
 }
 
 print_r($myArray);
+
+
+
