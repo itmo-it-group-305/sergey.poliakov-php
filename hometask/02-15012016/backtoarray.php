51f3d2674add38316126b04c7c55c99d1e7b4c46
Sergey Poliakov <ps.force@gmail.com>
2016-01-18 14:56:53 +0300

    Перевернул массив

---
+++ hometask/02-15012016/backtoarray.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergeypoliakov
+ * Date: 18.01.16
+ * Time: 13:59
+ */
+
+$myArray = [1, 2, 3, 8, 14, 89, 45];
+$length = count($myArray) - 1;
+
+for ($i = 0; $i <= $length/2; $i++) {
+    $temp = $myArray[$i];
+    $myArray[$i] = $myArray[$length - $i];
+    $myArray[$length - $i] = $temp;
+}
+
+print_r($myArray);
